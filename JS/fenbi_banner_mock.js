eab89bd00ea17b60fac7eb0edc26358c6156f967
ZZ1124 <120290608+ZZ1124@users.noreply.github.com>
2026-01-12 22:22:31 +0800

    Update fenbi_banner_mock.js

--- JS/fenbi_banner_mock.js
+++ JS/fenbi_banner_mock.js
@@ -1,32 +1,32 @@
-$done({
-  status: 200,
-  headers: {
-    "Content-Type": "application/json; charset=utf-8"
-  },
-  body: JSON.stringify({
-    "code": 1,
-    "msg": "",
-    "data": {
-      "positionResourceType": 1,
-      "payload": {
-        "items": [
-          {
-            "id": 17868,
-            "courseSetId": 2,
-            "content": "妹子图",
-            "imageUrl": "https://i.im.ge/2025/11/24/4DrVp0.mm.png",
-            "url": "null",
-            "redirectType": 1,
-            "ordinal": 8,
-            "createdTime": 1746799144383,
-            "startTime": 1759850171000,
-            "endTime": 4070880000000,
-            "type": 0,
-            "bizType": 4
-          }
-        ],
-        "count": 1
-      }
+let obj = JSON.parse($response.body);
+
+obj = {
+  code: 1,
+  msg: "",
+  data: {
+    positionResourceType: 1,
+    payload: {
+      items: [
+        {
+          id: 17868,
+          courseSetId: 2,
+          content: "妹子图",
+          imageUrl: "https://i.im.ge/2025/11/24/4DrVp0.mm.png",
+          url: "null",
+          redirectType: 1,
+          ordinal: 8,
+          createdTime: 1746799144383,
+          startTime: 1759850171000,
+          endTime: 4070880000000,
+          type: 0,
+          bizType: 4
+        }
+      ],
+      count: 1
     }
-  })
+  }
+};
+
+$done({
+  body: JSON.stringify(body)
 });
