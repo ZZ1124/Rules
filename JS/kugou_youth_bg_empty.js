0abdb3bef130d06614456efcbdc7c78031272b6f
ZZ1124 <120290608+ZZ1124@users.noreply.github.com>
2026-01-19 00:43:18 +0800

    Update kugou_youth_bg_empty.js

--- JS/kugou_youth_bg_empty.js
+++ JS/kugou_youth_bg_empty.js
@@ -1,8 +1,7 @@
-let body = JSON.parse($response.body);
-
-if (body.data && Array.isArray(body.data.list)) {
-  body.data.list = [];  // 清空背景图片列表
-  body.data.total = 0;  // 总数设为0
-}
-
-$done({ body: JSON.stringify(body) });
+let emptyBody = {
+  "error_msg": "",
+  "data": { "list": [], "total": 0 },
+  "status": 1,
+  "error_code": 0
+};
+$done({ body: JSON.stringify(emptyBody) });
