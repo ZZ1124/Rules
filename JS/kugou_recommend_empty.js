530183f078117529e4d21d20b84e69ff9a57fa30
ZZ1124 <120290608+ZZ1124@users.noreply.github.com>
2026-01-19 00:53:32 +0800

    Update kugou_recommend_empty.js

--- JS/kugou_recommend_empty.js
+++ JS/kugou_recommend_empty.js
@@ -2,6 +2,7 @@ let body = JSON.parse($response.body);
 
 if (body.data && Array.isArray(body.data.info)) {
   body.data.info = [];  // 清空推荐歌曲列表，让板块为空或隐藏
+  body.data.retry = 0 ;
 }
 
 $done({ body: JSON.stringify(body) });
